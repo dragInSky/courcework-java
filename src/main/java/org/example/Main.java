0d315ce86b37c1291354ba832385b66370f00c7b
YuriProduction <jurexda@yandex.ru>
2023-03-19 20:40:52 +0500

    Test commit in Jurex branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
 
   public static void main(String[] args) {
-    String[] words = {"хорда", "Макс", "Марфицына"};
+    String[] words = {"хорда", "Макс"};
 
     CrosswordGenerator CG = new CrosswordGenerator(words);
 
